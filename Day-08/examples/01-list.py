426254e61208435c4df2b43466a63eb691c91ffa
I_MNA <ahmad.nadim144@gmail.com>
2024-10-25 20:51:09 +0530

    Day-08 updated 01-list.py

--- Day-08/examples/01-list.py
+++ Day-08/examples/01-list.py
@@ -12,3 +12,10 @@ print(s1);
 print(s2);
 print(s3);
 print(s4);
+
+# list
+student_names = ["Md", "Nadim", "Ahmad", "Supaul"]
+print(student_names)
+
+buckets_names = ["Md_s3_buckets", "Nadim_s3_buckets", "Ahmad_s3_buckets", "Supaul_s3_buckets"]
+print(buckets_names)
